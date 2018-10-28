1b0b0193d3f8d5c3c0030c83915505851f865d94
jocvegar <jocvegar@gmail.com>
2018-10-28 15:58:59 -0600

    Adds title helpers and controller tests

--- test/controllers/bienvenido_controller_test.rb
+++ test/controllers/bienvenido_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class BienvenidoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get bienvenido_index_url
-    assert_response :success
-  end
+	def setup
+		@title = "MediNota"
+	end
+
+	test "should get index" do
+		get bienvenido_index_url
+		assert_response :success
+		assert_select "title", "Inicio | #{@title}"
+	end
+
+	test "should get root" do
+		get root_path
+		assert_response :success
+		assert_select "title", "Inicio | #{@title}"
+	end
 
 end
