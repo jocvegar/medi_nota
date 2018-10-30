2d5453c42aa169965770837dd828804cbe2cd265
jocvegar <jocvegar@gmail.com>
2018-10-29 22:28:11 -0600

    adds validation

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,3 +1,7 @@
 class Paciente < ApplicationRecord
 	belongs_to :hospital
+
+	validates_presence_of :nombre, :edad, :fecha_de_ingreso, :antecedentes, :diagnostico, :plan, :genero, :camilla, :message => "no puede estar vacio."
 end
+
+
